63626c70e189f553737047bfa84d9c266a799313
Kauê de Magalhães <61990018+kkkaue@users.noreply.github.com>
2022-01-23 11:17:34 -0300

    Add files via upload

---
+++ pilha(stack).py
@@ -0,0 +1,62 @@
+from no import Node
+
+# inserir na pilha
+# remover da pilha
+# observar o topo da pilha
+
+class Pilha:
+    def __init__(self):
+        self.top = None
+        self._size = 0
+
+    def push(self,elem):
+        # insere um elemento na pilha
+        node = Node(elem)
+        node.next = self.top
+        self.top = node
+        self._size = self._size + 1
+
+    def pop(self):
+        # remove o elemento do topo da pilha
+        if self._size > 0:
+            node = self.top
+            self.top = self.top.next
+            self._size = self._size - 1
+            return node.data
+        raise IndexError("The stack is empty")
+
+
+    def peek(self):
+        # retorna o topo sem remover
+        if self._size > 0:
+            return self.top.data
+        raise IndexError("The stack is empty")
+
+
+
+    def __len__(self):
+        """Retorna o tamanho da lista"""
+        return self._size
+
+    def __repr__(self):
+        r = ""
+        pointer = self.top
+        while(pointer):
+            r = r + str(pointer.data) + "\n"
+            pointer = pointer.next
+        return r
+
+    def __str__(self):
+        return self.__repr__()
+
+if __name__ == '__main__':
+    pilha = Pilha()
+    print(len(pilha))
+    pilha.push(1)
+    pilha.push(2)
+    pilha.push("python")
+    pilha.push(True)
+    pilha.push(1.2)
+
+    print(pilha)
+    print(len(pilha))
